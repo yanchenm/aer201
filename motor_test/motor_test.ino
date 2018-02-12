dc2ddf76b19f8c1f8ea99b2a446961f788fb5137
Yanchen Ma <yanchenm@gmail.com>
2018-02-12 12:44:22 -0500

    Servo test code

--- motor_test/motor_test.ino
+++ motor_test/motor_test.ino
@@ -1,17 +1,26 @@
-int servoPin = 3;
-int pulse = 2;
+#include <Servo.h>
+
+Servo servo;
+
+int servoPin = 9;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(servoPin, OUTPUT);
   Serial.begin(9600);
+  servo.attach(servoPin);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  digitalWrite(servoPin, HIGH);
-  delay(1);
-  digitalWrite(servoPin, LOW);
-  delay(19);
+  servo.write(0);
+  delay(1000);
+  servo.write(45);
+  delay(1000);
+  servo.write(90);
+  delay(1000);
+  servo.write(135);
+  delay(1000);
+  servo.write(180);
+  delay(1000);
 }
